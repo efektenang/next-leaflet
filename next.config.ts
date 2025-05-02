45fe30f50eda46528d17f8a7e2f441339f4dd465
Muhammad Arizky <arizkymuhammad46@gmail.com>
2025-05-02 15:26:58 +0700

    chore: add new package for leaflet configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["https://emerging-wired-killdeer.ngrok-free.app"],
 };
 
 export default nextConfig;
